5a739a3f34fd4f21d48a738ed15966682a594a49
Valerii <87275444+vvoinarovych@users.noreply.github.com>
2024-04-28 12:18:57 +0200

    init

---
+++ src/main/java/com/vvoinarovych/tasktrackerbackend/TaskTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vvoinarovych.tasktrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerBackendApplication.class, args);
+    }
+
+}
